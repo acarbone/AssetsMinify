8974073c7ee59f4cf99138115efba551f6704b4b
Alessandro Carbone <ale.carbo@gmail.com>
2013-04-14 21:03:32 +0200

    Defined tests for extracting css and js from the wp queue

--- tests/AMInitTests.php
+++ tests/AMInitTests.php
@@ -34,4 +34,59 @@ class AMInitTests extends WP_UnitTestCase {
 			$this->assertFalse( filemtime($filepath) <= time() - 864000 );
 	}
 
+	public function testExtractStyles() {
+		global $wp_styles;
+		wp_enqueue_style( 'twentytwelve-style', get_stylesheet_uri() );
+
+		//do_action( 'wp_print_styles' );
+		$this->assertFalse( empty($wp_styles) );
+		$this->assertTrue( is_array($wp_styles->queue) );
+
+	}
+
+	public function testStylesQueueManagement() {
+		global $wp_styles;
+		wp_enqueue_style( 'twentytwelve-style', get_stylesheet_uri() );
+
+		$external = 0;
+		$_SERVER['SERVER_NAME'] = 'wordpress.local';
+		foreach( $wp_styles->queue as $handle ) {
+			$style = str_replace( "http://{$_SERVER['SERVER_NAME']}", "", $wp_styles->registered[$handle]->src );
+			if ( strpos($style, "http") === 0 || strpos($style, "//") === 0 ) {
+				$external++;
+				continue;
+			}
+			$wp_styles->dequeue( $handle );
+		}
+		$this->assertEquals( count($wp_styles->queue), $external );
+	}
+
+	public function testExtractScripts() {
+		global $wp_scripts;
+		wp_enqueue_script( 'twentytwelve-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '1.0', true );
+
+		//do_action( 'wp_print_styles' );
+		$this->assertFalse( empty($wp_scripts) );
+		$this->assertTrue( is_array($wp_scripts->queue) );
+
+	}
+
+	public function testScriptsQueueManagement() {
+		global $wp_scripts;
+		wp_enqueue_script( 'twentytwelve-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '1.0', true );
+		wp_enqueue_script( 'script4', '//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', array(), '1.0', true );
+
+		$external = 0;
+		$_SERVER['SERVER_NAME'] = 'wordpress.local';
+		foreach( $wp_scripts->queue as $handle ) {
+			$style = str_replace( "http://{$_SERVER['SERVER_NAME']}", "", $wp_scripts->registered[$handle]->src );
+			if ( strpos($style, "http") === 0 || strpos($style, "//") === 0 ) {
+				$external++;
+				continue;
+			}
+			$wp_scripts->dequeue( $handle );
+		}
+		$this->assertEquals( count($wp_scripts->queue), $external );
+	}
+
 }
