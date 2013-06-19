8e94a3cca643fcee48b33496cf124cd97d3d2cda
Alessandro Carbone <alessandrocarbone@artera.it>
2013-06-19 16:24:33 +0200

    Updated plugin's wp info

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@ Plugin Name: Assets Minify
 Plugin URI: https://github.com/acarbone/AssetsMinify
 Description: WordPress plugin to minify JS and CSS assets.
 Author: Alessandro Carbone
-Version: 1.1.3
+Version: 1.1.4
 Author URI: http://www.artera.it
 */
 
